bdfd23854030416e842bb41a7da67c67b1e58ae9
Wafflekillr <Ethan_2001@yahoo.com>
2018-03-23 15:39:46 -0500

    AI Waypoints Updated

---
+++ Source/Battle_Grounds/Public/PatrolRoute.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolRoute.generated.h"
+
+/**
+* A "Route Card" To Help AI Choose Their Next Waypoint
+*/
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLE_GROUNDS_API UPatrolRoute : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	TArray<AActor*> GetPatrolPoints() const;
+
+private:
+	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+		TArray<AActor*> PatrolPoints;
+
+		
+	
+};
